bda3e8a7771aa02d0ed55481fbea21f6e7ab3a05
zachtaylorcsc <zachtaylorcsc@gmail.com>
2025-04-23 11:45:07 -0500

    Update global.h
    moved booth's instructions to global

--- TigerGame/global.h
+++ TigerGame/global.h
@@ -6,15 +6,32 @@
 #define TIGERGAME_GLOBAL_H
 
 
-enum Color_t { RED, BLUE };
+enum Color_t {RED, BLUE};
 
-struct Point_t {
-    int row, col;
+struct Point_t{
+	int row, col;
 };
 
-struct Token_t {
-    Color_t color;
-    Point_t location;
+struct Token_t{
+	Color_t color;
+	Point_t location;
 };
 
+struct Move_t{
+	Token_t token;
+	Point_t destination;
+};
+
+Move_t  Move(vector<Token_t>, Color_t turn);
+
+/*
+ * Each group will implement the Move function and
+ * submit their code using the Upload Site.
+ * 
+ * You should name your move function: Move_GroupName.
+ * 
+ * This will create a function prototype something like:
+ * 
+ *  Move_t Move_GroupName(vector<Token_t>, Color_t turn);
+ */
 #endif //TIGERGAME_GLOBAL_H
